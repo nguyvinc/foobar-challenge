42c4cc6e7f94523b9c4a931927d8313511e1c1e5
Vincent Nguyen <29743521+nguyvinc@users.noreply.github.com>
2020-08-15 20:20:04 -0700

    Update solution.py

--- level-1/solution.py
+++ level-1/solution.py
@@ -2,21 +2,22 @@ def solution(data, n):
     occurences = {}
     remove = {}
     new_list = []
+    
     # Count number of occurences for each number in list
     for value in data:
-        if str(value) in occurences:
+        if str(value) in occurences:    # If key already exists, increment occurrence
             occurences[str(value)] = occurences[str(value)] + 1
-        else:
+        else:                           # Else add key, set to 1
             occurences[str(value)] = 1
             
     # Check occurences for each number in the list, add to remove if greater than n
     for value, occur in occurences.items():
         if occur > n:
-            remove[str(value)] = value
+            remove[str(value)] = 1
             
-    # Remove numbers from the list
+    # Copy over non-removed numbers to new list
     for value in data:
-        if not str(value) in remove:
+        if not str(value) in remove:    # If value is not in remove, add to new list
             new_list.append(value)
             
     return new_list
